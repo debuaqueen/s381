10e0707b86af9567ae2607cb9a1d433413c19bcc
debuaqueen <154677972+debuaqueen@users.noreply.github.com>
2025-11-21 23:07:37 +0800

    Update Student.js

--- models/Student.js
+++ models/Student.js
@@ -4,7 +4,13 @@ const studentSchema = new mongoose.Schema({
   name: { type: String, required: true },
   studentId: { type: String, required: true, unique: true },
   age: { type: Number, required: true },
+  gender: { type: String, enum: ['Male', 'Female'], required: true },  // ← NEW
   major: { type: String, required: true }
 });
 
+
 module.exports = mongoose.model('Student', studentSchema);
+
+
+
+
